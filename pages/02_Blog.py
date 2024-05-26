f724fb13e18983b9c6178eefdabbb6e835623dad
Juan G. Martins <102706481+JuanGonccalves@users.noreply.github.com>
2024-05-26 20:51:06 -0300

    02_Blog.py

--- pages/02_Blog.py
+++ pages/02_Blog.py
@@ -8,7 +8,7 @@ from PIL import Image
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "Juan Gonçalves Martins.pdf"
-profile_pic = current_dir / "assets" / "profile.jpg"
+profile_pic = current_dir / "assets" / "Profile.jpg"
 
 
 # --- GENERAL SETTINGS ---
