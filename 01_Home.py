73ca3d5c8b9a910b8322ffd620e247ea3e06adfa
Juan G. Martins <102706481+JuanGonccalves@users.noreply.github.com>
2024-05-31 10:16:02 -0300

    01_Home.py

--- 01_Home.py
+++ 01_Home.py
@@ -13,7 +13,7 @@ font = "sans serif"
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "styles" / "main.css"
 resume_file = current_dir / "assets" / "Juan Gonçalves Martins.pdf"
-profile_pic_path = current_dir / "assets" / "profile.jpg"
+profile_pic_path = current_dir / "assets" / "Profile.jpg"
 
 # --- GENERAL SETTINGS ---
 PAGE_TITLE = "Digital CV | Juan G. Martins"
